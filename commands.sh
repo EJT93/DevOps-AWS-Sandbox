4cce5f6042a1e42a461fc725406e87a4cbd24b89
EJT93 <elijahtorrence+github1@gmail.com>
2023-04-21 22:10:09 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -4,15 +4,21 @@
 sudo yum update -y
 
 # Install Docker
-sudo amazon-linux-extras install docker -y
+sudo yum install docker -y
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -a -G docker ec2-user
 
+# Enable Amazon Linux Extras and JDK
+sudo bash -c 'echo -e "[amzn2extra]\nname=Amazon Linux 2 Extras Repository\nbaseurl=https://amazonlinux2-repositories.s3.amazonaws.com/amazon-linux-extras\ngpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-amzn2\ngpgcheck=1\nenabled=1" > /etc/yum.repos.d/amzn2-extras.repoTEST'
+sudo yum update -y
+sudo amazon-linux-extras enable corretto-11
+sudo yum install -y fontconfig java-11-amazon-corretto
+
 # Install Jenkins
-sudo yum install java-1.8.0-openjdk-devel -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+sudo yum install fontconfig java-11-openjdk -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
