daa39434860660930de68d31b460b6628392869f
EJT93 <elijahtorrence+github1@gmail.com>
2023-04-21 22:30:44 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -18,7 +18,6 @@ sudo yum install -y fontconfig java-11-amazon-corretto
 # Install Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
-sudo yum install fontconfig java-11-openjdk -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
